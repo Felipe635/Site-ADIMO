da7bda7758dda8017a43331345c334ac304321db
Felipe635 <102757641+Felipe635@users.noreply.github.com>
2023-05-18 00:09:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var email = document.getElementById("email");
-var senha = document.getElementById("senha");
 
-if(email.value == "adm" && senha.value == "adm123"){
+
+function logar(){
+
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+
+if(email == "adm"){
+    alert("ADM entrou!");
+    location.href = "index.html"
 //abri a pagina do adm que tema atela de cadastro
 }else{
-    
+    alert("cliente entrou");
+}
 }
