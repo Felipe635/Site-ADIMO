43b4a8cf76555da452aae2ecb92a0c52de494f1a
Felipe635 <102757641+Felipe635@users.noreply.github.com>
2023-05-23 23:49:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,20 @@ function logar(){
 
 if(email == "adm"){
     alert("ADM entrou!");
-    location.href = "index.html"
+    location.href = "adm.html"
 //abri a pagina do adm que tema atela de cadastro
 }else{
     alert("cliente entrou");
 }
+
+}
+
+function addFav2 (){
+    var linha2 = document.getElementById("linha2");
+    linha2.classList.toggle('linha2-on');
+}
+
+function addFav3 (){
+    var linha3 = document.getElementById("linha3");
+    linha3.classList.toggle('linha3-on');
 }
